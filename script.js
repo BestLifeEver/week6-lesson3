63cf00593b5b2e22f3e8b2ed199cf135d446b53a
BestLifeEver <100959834+BestLifeEver@users.noreply.github.com>
2022-07-05 20:43:54 -0400

    sweeping changes
    Moved open weather api project to this repository from code sandbox

--- script.js
+++ script.js
@@ -1,1 +1,75 @@
-alert("netify me")
+// Current day and time data
+let now = new Date();
+let days = [
+  "Sunday",
+  "Monday",
+  "Teusday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+let day = days[now.getDay()];
+let hours = now.getHours();
+let minutes = now.getMinutes();
+
+let h2 = document.querySelector("h2");
+h2.innerHTML = `${day} ${hours}:${minutes}`;
+
+// API  data
+function showTemperature(response) {
+  let city = response.data.name;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = `${city}`;
+  let description = response.data.weather.[0].description;
+  let descriptionElement = document.querySelector("#temperature-description");
+descriptionElement.innerHTML = `${description}`
+  let temperature = Math.round(response.data.main.temp);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = `${temperature}`;
+  let humidity = response.data.main.humidity;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = `Humidity: ${humidity}%`
+  let wind = response.data.wind.speed;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = `Wind: ${wind} km/h`;
+}
+
+
+
+  
+function searchCity(city) {
+let apiKey = "a08c40eb692927cb65bcfe8640547b63";
+let units = "metric"
+let apiUrl =
+  `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+
+axios.get(apiUrl).then(showTemperature);
+}
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  searchCity(city);
+}
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", handleSubmit);
+
+function searchLocation(position) {
+let apiKey = "a08c40eb692927cb65bcfe8640547b63";
+
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${
+    position.coords.latitude
+  }&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(showTemperature);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+let currentLocationButton = document.querySelector("#current-location-button");
+currentLocationButton.addEventListener("click", getCurrentLocation);
+
+searchCity("Kenneth City");
